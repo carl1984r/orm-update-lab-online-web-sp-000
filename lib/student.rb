04b1fb1f74f125e727794de260c7bdb147212ad7
Bry Cox <carl.cc@gmail.com>
2019-05-11 06:33:39 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -25,8 +25,14 @@ class Student
 
   end
 
-  # Remember, you can access your database connection anywhere in this class
-  #  with DB[:conn]
+  def self.drop_table
+    
+    sql = "DROP TABLE IF EXISTS students"
+    DB[:conn].execute(sql)
+    
+  end
+
+  
 
 
 end
